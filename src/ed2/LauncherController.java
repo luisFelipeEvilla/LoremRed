8bcc7e8aef47c6ad076f3677a2c0fe7c0ee06402
Juan Camilo <Juan Camilo@Usuario-PC>
2020-09-25 22:47:14 -0500

    Version casi final

---
+++ src/ed2/LauncherController.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ed2;
+
+import Screens.Home;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.layout.GridPane;
+import javafx.util.Duration;
+import loremred.LoremRed;
+
+/**
+ * FXML Controller class
+ *
+ * @author Juan Camilo
+ */
+public class LauncherController implements Initializable {
+
+    @FXML
+    private GridPane gridPane;
+    @FXML
+    private Hyperlink hipIniciaSesion;
+    @FXML
+    private Hyperlink hipRegistrate;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        animacionInicio();
+    }
+
+    @FXML
+    private void hipIniciaSesion(ActionEvent event) {
+        Timeline timeline = new Timeline();
+        KeyValue kv = new KeyValue(gridPane.layoutXProperty(), -400, Interpolator.EASE_OUT);
+        KeyFrame kf = new KeyFrame(Duration.millis(500), kv);
+        timeline.getKeyFrames().add(kf);
+        timeline.play();
+    }
+
+    @FXML
+    private void hipRegistrate(ActionEvent event) {
+        Timeline timeline = new Timeline();
+        KeyValue kv = new KeyValue(gridPane.layoutXProperty(), 0, Interpolator.EASE_OUT);
+        KeyFrame kf = new KeyFrame(Duration.millis(500), kv);
+        timeline.getKeyFrames().add(kf);
+        timeline.play();
+    }
+
+    private void animacionInicio() {
+        Timeline timeline = new Timeline();
+        KeyValue kv = new KeyValue(gridPane.getColumnConstraints().get(1).prefWidthProperty(), 400, Interpolator.EASE_OUT);
+        KeyFrame kf = new KeyFrame(Duration.millis(500), kv);
+        timeline.getKeyFrames().add(kf);
+        timeline.play();
+    }
+
+    @FXML
+    private void botIniciarSesion(ActionEvent event) {
+        Home ui = new Home(LoremRed.usuarios);
+        ui.setVisible(true);
+    }
+
+}
--- src/loremred/LoremRed.java
+++ src/loremred/LoremRed.java
@@ -5,22 +5,8 @@
  */
 package loremred;
 
-import Interfaz.Launcher;
-import Screens.Home;
-import Screens.Signin;
-import java.io.BufferedReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import utils.CustomList.Lista;
 import utils.CustomList.Nodo;
-import utils.Utils;
 
 /**
  *
@@ -52,20 +38,4 @@ public class LoremRed {
     public void removeUser(int id) {
         usuarios.remove(id);
     }
-
-    /*
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        try {
-            Launcher launcher = new Launcher();
-            launcher.setVisible(true);
-            LoremRed app = new LoremRed();
-            LoremRed.usuarios = utils.Utils.saveData();
-            launcher.animar();
-        } catch (IOException ex) {
-            Logger.getLogger(LoremRed.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
 }
